6d6149513ec64f543f2c9e94646eb0f2fef43f24
Alissa Nguyen <alissa.nguyen1211@gmail.com>
2021-10-13 23:17:21 -0700

    .

---
+++ frontend/js/pages/CustomersDetails.jsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import api from '../store/api';
+const CustomersDetails = () => {
+  
+  return (
+    <div>
+      <p>Hi</p>
+    </div>
+  );
+};
+
+export default CustomersDetails;
